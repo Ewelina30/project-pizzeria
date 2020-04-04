09f9ed706f22b4d86570aecfe7a5a9e96ff443e2
Ewelina Słyk <ewelinaslyk@wp.pl>
2020-04-04 13:36:47 +0200

    Add src/js/script.js

--- src/js/script.js
+++ src/js/script.js
@@ -53,27 +53,101 @@
   };
 
   class Product{
-    constructor(){
+    constructor(id,data){
       const thisProduct = this;
 
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
       console.log('new Product', thisProduct);
-    }
+
+      thisProduct.initAccordion();
+
   }
+
+  renderInMenu(){
+    const thisProduct = this;
+
+    /* generate HTML based on template */
+    const generateHTML = templates.menuProduct(thisProduct.data);
+
+    /* create element using utils.createElementFromHTML */
+    thisProduct.element = utils.createDOMFromHTML(generateHTML);
+
+    /* find menu container */
+    const menuContainer = document .querySelector(select.containerOf.menu);
+
+    /* add element to menu */
+    menuContainer.appendChild(thisProduct.element);
+  }
+
+
+initAccordion(){
+  const thisProduct = this;
+
+  /* find the clickable trigger (the element that should react to clicking) */
+  /* znajdź klikalny wyzwalacz (element, który powinien zareagować na kliknięcie) */
+   const clicableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+   console.log(clicableTrigger);
+
+    /* START: click event listener to trigger */
+    /*START: kliknij detektor zdarzeń, aby uruchomić*/
+    thisProduct.accordionTrigger.addEventListener('click', function(event){
+
+      /* prevent default action for event */
+      /* zapobiec domyślnej akcji dla zdarzenia */
+          event.preventDefault();
+
+      /* toggle active class on element of thisProduct */
+      /* przełącz aktywną klasę na elemencie thisProduct */
+      thisProduct.element.classList.toggle('active');
+
+      /* find all active products */
+      /* znajdź wszystkie aktywne produkty */
+      const allActiveProducts = document.querySelectorAll('article.active');
+
+      /* START LOOP: for each active product */
+      /* PĘTLA STARTOWA: dla każdego aktywnego produktu */
+      for(let activeProduct of allActiveProducts ){
+
+        /* START: if the active product isn't the element of thisProduct */
+        /* START: jeśli aktywny produkt nie jest elementem tego produktu */
+        if( activeProduct !== thisProduct.element){
+
+          /* remove class active for the active product */
+          /* usuń klasę aktywną dla aktywnego produktu */
+          activeProduct.classList.remove('active');
+          console.log(activeProduct);
+        }
+        /* END: if the active product isn't the element of thisProduct */
+        /* KONIEC: aktywnego produktu nie jest elementem tego produktu */
+      }
+
+      /* END LOOP: for each active product */
+      /* PĘTLA KOŃCOWA: dla każdego aktywnego produktu */
+      });
+    /* END: click event listener to trigger */
+    /* END: kliknij detektor zdarzeń, aby uruchomić */
+  }
+}
+
 
   const app = {
     initMenu: function(){
-      const testProduct = new Product();
-      const  thisApp = this;
+      const thisApp = this;
 
+      console.log('thisApp:', thisApp.data);
 
-      console.log('testProduct', testProduct);
-      console.log('thisApp.data:', thisApp.data);
+      for (let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
     },
 
     initData: function(){
       const thisApp = this;
 
-      thispApp.data = dataSource;
+      thisApp.data = dataSource;
     },
 
     init: function(){
