ba8ebe572c2d4ea7cba7655dbd5bf9c26ac805a5
Ewelina Słyk <ewelinaslyk@wp.pl>
2020-04-27 19:41:46 +0200

    Add quantity widget

--- src/js/script.js
+++ src/js/script.js
@@ -290,18 +290,35 @@ getElements(element){
   thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
 }
 
-setValue(){
+setValue(value){
   const thisWidget = this;
 
   const newValue = parseInt(value);
 
-
+ console.log(settings);
   /*TODO: Add validation*/
   /* DO ZROBIENIA: Dodaj weryfikację */
 
   thisWidget.value = newValue;
   thisWidget.input.value = thisWidget.value;
-  }
+}
+
+initActions{
+  const thisWidget = this;
+
+  thisWidget.input.addEventListener('change', function(){
+    thisWidget.setValue(thisWidget.input.value);
+  });
+
+  thisWidget.linkDecrease.addEventListener('click', function(event){
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value - 1);
+  });
+  thisWidget.linkIncrease.addEventListener('click', function(event){
+         event.preventDefault();
+     thisWidget.setValue(thisWidget.value + 1);
+   });
+ }
 }
 
   const app = {
