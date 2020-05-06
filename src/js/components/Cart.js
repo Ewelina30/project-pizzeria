1e932106d3c63d8a9b04e352c95db4bbe903f473
Ewelina Słyk <ewelinaslyk@wp.pl>
2020-05-06 19:06:57 +0200

    Add src/js/components/Cart.js

--- src/js/components/Cart.js
+++ src/js/components/Cart.js
@@ -1,3 +1,9 @@
+import {select} from '../settings.js';
+import {settings, classNames, templates} from '../settings.js';
+import {utils} from '../utils.js';
+import {CartProduct} from './CartProduct.js';
+
+
 class Cart{
   constructor(element){
     const thisCart = this;
