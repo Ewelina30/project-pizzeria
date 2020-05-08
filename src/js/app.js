82f5f330d58e7c8713283b5fb4a2855d9d398206
Ewelina Słyk <ewelinaslyk@wp.pl>
2020-05-09 00:00:57 +0200

    Add src/js/app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,85 @@
 /* eslint-disable no-undef */
 /* global Handlebars, utils, dataSource */// eslint-disable-line no-unused-vars
 
-import {settings, select} from './settings.js';
+import {settings, select,classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import {Booking} from './components/booking.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+
+    }
+
+    thisApp.activatePages(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /*get page id from href atribute*/
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /*run thisApp.activatePage with that id*/
+        thisApp.activatePages(id);
+
+        /*change URL hash*/
+        window.location.hash = '#/' + id;
+
+      });
+    }
+  },
+
+  initBooking: function (){
+    const thisApp = this;
+
+    const bookingWrapper = document.querySelector(select.containerOf.booking);
+
+    thisApp.booking = new Booking(bookingWrapper);
+  },
+
+  activatePages: function(pageId){
+    const thisApp = this;
+
+    /*add class "active" to maching pages, remove from non-matching*/
+    /* dodaj klasę „aktywną” do stron obróbki, usuń z niepasujących */
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    /*add class "active" to maching links, remove from non-matching*/
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initMenu: function () {
     const thisApp = this;
     // console.log('thisApp.data:', thisApp.data);
 
     for (let productData in thisApp.data.products) {
-      new Product(thisApp.data.products[productData].id,
-        thisApp.data.products[productData]);
+      new Product(
+        thisApp.data.products[productData].id,
+        thisApp.data.products[productData]
+      );
     }
   },
 
@@ -61,9 +128,11 @@ const app = {
     //console.log('settings:', settings);
     //console.log('templates:', templates);
 
-   
+    
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
     thisApp.initMenu();
   },
 };
