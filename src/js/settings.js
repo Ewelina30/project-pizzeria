0beb0812b52adeb757c45c75460f7259eb28769d
Ewelina Słyk <ewelinaslyk@wp.pl>
2020-05-12 22:54:08 +0200

    Add src/js/settings.js

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,4 +1,4 @@
-/*global Handlebars*/ 
+/* global Handlebars */
 
 export const select = {
   templateOf: {
@@ -30,15 +30,15 @@ export const select = {
       input: 'input.amount',
       linkDecrease: 'a[href="#less"]',
       linkIncrease: 'a[href="#more"]',
-      datePicker: {
-        wrapper: '.date-picker',
-        input: `input[name="date"]`,
-      },
-      hourPicker: {
-        wrapper: '.hour-picker',
-        input: 'input[type="range"]',
-        output: '.output',
-      },
+    },
+    datePicker: {
+      wrapper: '.date-picker',
+      input: `input[name="date"]`,
+    },
+    hourPicker: {
+      wrapper: '.hour-picker',
+      input: 'input[type="range"]',
+      output: '.output',
     },
   },
   cart: {
@@ -126,9 +126,10 @@ export const templates = {
   menuProduct: Handlebars.compile(
     document.querySelector(select.templateOf.menuProduct).innerHTML
   ),
+  bookingWidget: Handlebars.compile(
+    document.querySelector(select.templateOf.bookingWidget).innerHTML
+  ),
   cartProduct: Handlebars.compile(
     document.querySelector(select.templateOf.cartProduct).innerHTML
-  ),
-  bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML
   ),
 };
