e618f75c68fdacae398ad774dc0f2015be7e4f54
Ewelina Słyk <ewelinaslyk@wp.pl>
2020-05-12 20:34:19 +0200

    Add src/js/components/CartProduct.js

--- src/js/components/CartProduct.js
+++ src/js/components/CartProduct.js
@@ -1,8 +1,8 @@
 import {select} from '../settings.js';
-import {AmountWidget} from './AmountWidget.js';
+import AmountWidget from './AmountWidget.js';
 
-class CartProduct{
-  constructor(menuProduct,element){
+class CartProduct {
+  constructor(menuProduct, element) {
     const thisCartProduct = this;
 
     thisCartProduct.id = menuProduct.id;
@@ -14,65 +14,76 @@ class CartProduct{
 
     thisCartProduct.getElements(element);
     thisCartProduct.initAmountWidget();
-      
-    console.log('new CartProduct', thisCartProduct);
-    console.log('productData', menuProduct);
+    thisCartProduct.initActions();
   }
 
-  getElements(element){
+  getElements(element) {
     const thisCartProduct = this;
 
     thisCartProduct.dom = {};
     thisCartProduct.dom.wrapper = element;
-    thisCartProduct.dom.amountWidget = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.amountWidget);
-    thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.price);
-    thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
-    thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
+    thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(
+      select.cartProduct.price
+    );
+    thisCartProduct.dom.amountWidget = thisCartProduct.dom.wrapper.querySelector(
+      select.cartProduct.amountWidget
+    );
+    thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(
+      select.cartProduct.edit
+    );
+    thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(
+      select.cartProduct.remove
+    );
+  }
 
-    thisCartProduct.dom.remove.addEventListener('click', function(event) {
-      event.preventDefault();
-      thisCartProduct.remove();
+  initAmountWidget() {
+    const thisCartProduct = this;
+
+    thisCartProduct.amountWidget = new AmountWidget(
+      thisCartProduct.dom.amountWidget
+    );
+    thisCartProduct.dom.amountWidget.addEventListener('updated', function () {
+      thisCartProduct.amount = thisCartProduct.amountWidget.value;
+      thisCartProduct.price =
+        thisCartProduct.priceSingle * thisCartProduct.amount;
+      thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
     });
   }
 
-  remove(){
+  remove() {
     const thisCartProduct = this;
-  
+
     const event = new CustomEvent('remove', {
       bubbles: true,
       detail: {
         cartProduct: thisCartProduct,
       },
     });
-  
+
     thisCartProduct.dom.wrapper.dispatchEvent(event);
   }
 
-  initAmountWidget(){
+  initActions() {
     const thisCartProduct = this;
-
-    thisCartProduct.amountWidget = new AmountWidget(thisCartProduct.dom.amountWidget);
-
-    thisCartProduct.dom.amountWidget.addEventListener('updated', function(){
-      thisCartProduct.amount = thisCartProduct.amountWidget.value;
-      thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount;
-      //console.log('thisCartProduct price is: ', thisCartProduct.price);
-      thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+    thisCartProduct.dom.edit.addEventListener('click', function (event) {
+      event.preventDefault();
+    });
+    thisCartProduct.dom.remove.addEventListener('click', function (event) {
+      event.preventDefault();
+      // console.log(thisCartProduct.dom.remove);
+      thisCartProduct.remove();
     });
   }
-
-  getData(){
+  getData() {
     const thisCartProduct = this;
-  
-    const productData = {
-      id: thisCartProduct.id,
-      price: thisCartProduct.price,
-      priceSingle: thisCartProduct.priceSingle,
-      amount: thisCartProduct.amount,
-      params: thisCartProduct.params,
-    };
-     
-    return productData;
+
+    return (
+      thisCartProduct.id,
+      thisCartProduct.price,
+      thisCartProduct.priceSingle,
+      thisCartProduct.amount,
+      thisCartProduct.params
+    );
   }
 }
 
